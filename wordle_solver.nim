dae71ac322375b211672281d1362f55eadf8f51c
Eino J. Oltedal <einoju@gmail.com>
2022-02-10 18:59:50 +0100

    Add function for getting 5 most commont letters

--- wordle_solver.nim
+++ wordle_solver.nim
@@ -1,3 +1,12 @@
+import std/[strformat, sequtils, tables]
 import wordlist
 
-echo words
+proc get_top_five_letters(wordseq: seq[string]): seq[char] = 
+  var alphabet = initCountTable[char]()
+  for word in wordseq:
+    for letter in word:
+      alphabet.inc(letter)
+  alphabet.sort
+  result =  toSeq(alphabet.keys)[0..4]
+
+echo get_top_five_letters(words)
