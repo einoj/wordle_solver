a6ac115ebd1c1f71d736483b43f8a1a841dfd98a
Eino J. Oltedal <einoju@gmail.com>
2022-02-11 23:41:25 +0100

    Add get_words_containing_letters function
    Return a linked list of words conatining the given sequence of letters.

--- wordle_solver.nim
+++ wordle_solver.nim
@@ -1,7 +1,7 @@
-import std/[strformat, sequtils, tables]
+import std/[strformat, sequtils, tables, lists, strutils]
 import wordlist
 
-proc get_top_five_letters(wordseq: seq[string]): seq[char] = 
+proc get_top_five_letters(wordseq: seq[string]): seq[char] =
   var alphabet = initCountTable[char]()
   for word in wordseq:
     for letter in word:
@@ -9,4 +9,20 @@ proc get_top_five_letters(wordseq: seq[string]): seq[char] =
   alphabet.sort
   result =  toSeq(alphabet.keys)[0..4]
 
-echo get_top_five_letters(words)
+
+proc get_words_containing_letters(letters: seq[char], wordseq: seq[string]): DoublyLinkedList[string] =
+  var list = initDoublyLinkedList[string]()
+  for word in wordseq:
+    var count = 0
+    for letter in letters:
+      if not (letter in word):
+        break
+      count += 1
+    if count == len(letters):
+      list.add(word)
+  result = list
+
+let top_five = get_top_five_letters(words)
+echo top_five
+echo get_words_containing_letters(top_five[0..3], words)
+
